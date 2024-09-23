e5b746f24bdab7ed1330a3b6839129e841c27e08
Zeyad Hisham Elsayed <zeyad20hisham@gmail.com>
2024-09-24 00:09:29 +0300

    #

--- EmbeddedC/ATmega32_Drivers/commonMacros.h
+++ EmbeddedC/ATmega32_Drivers/commonMacros.h
@@ -4,29 +4,28 @@
  * Description: Commonly used Macros
  * Author: Eng. Zeyad Hisham
  *******************************************************************************/
-
 #ifndef COMMONMACROS_H
 #define COMMONMACROS_H
 
-/* Set a specific bit in a register */
-#define SET_BIT(REG, BitNum) ((REG) |= (1u << (BitNum)))
+/* Set-Bit */
+#define SET_BIT(REG, BitNum) ((REG |= (1u << BitNum)))
 
-/* Clear a specific bit in a register */
-#define CLEAR_BIT(REG, BitNum) ((REG) &= ~(1u << (BitNum)))
+/* Clear-Bit */
+#define CLEAR_BIT(REG, BitNum) ((REG &= ~(1u << BitNum)))
 
-/* Toggle a specific bit in a register */
-#define TOGGLE_BIT(REG, BitNum) ((REG) ^= (1u << (BitNum)))
+/* Toggle-Bit*/
+#define TOGGLE_BIT(REG, BitNum) ((REG ^= (1u << BitNum)))
 
-/* Rotate bits to the right */
-#define ROR(REG, NUM) (((REG) >> (NUM)) | ((REG) << ((8 - (NUM)) & 0x07)))
+/* Rotate Right */
+#define ROR(REG, NUM) ((REG >> NUM) | (REG << (8 - NUM)))
 
-/* Rotate bits to the left */
-#define ROL(REG, NUM) (((REG) << (NUM)) | ((REG) >> ((8 - (NUM)) & 0x07)))
+/* Rotate Left*/
+#define ROL(REG, NUM) ((REG << NUM) | (REG >> (8 - NUM)))
 
-/* Check if a specific bit is set */
-#define BIT_IS_SET(REG, BIT) ((REG) & (1u << (BIT)))
+/* Bit is Set */
+#define BIT_IS_SET(REG, BIT) ((REG & (1u << BIT)))
 
-/* Check if a specific bit is clear */
-#define BIT_IS_CLEAR(REG, BIT) (!((REG) & (1u << (BIT))))
+/* Bit is Clear */
+#define BIT_IS_CLEAR(REG, BIT) (!(REG & (1u << BIT)))
 
 #endif
